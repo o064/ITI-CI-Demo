1bde8485b616877c31a2239f11e1ffff7439855b
omar <om039919@gmail.com>
2025-07-19 01:42:56 +0300

    test ci workfolw

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, ITI!"
+    return "Hello, ITI!... Hello ,omar !"
 
 if __name__ == '__main__':
     app.run()
